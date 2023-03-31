2c09aae023e7602be9703a947783d285b5f56bfb
Bhavya-coder123 <100366182+Bhavya-coder123@users.noreply.github.com>
2023-03-31 12:21:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+Webcam.set({
+    width:370,
+    height:310,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById('result').innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+
+    });
+}
+
+console.log("ml5 version", ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ArlJeunAn/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log('model is loaded');
+}
+
+
+function speak(){
+    var synth=window.speechSynthesis;
+    sd='The prediction is'+prediction;
+    var utterThis=new SpeechSynthesisUtterance(sd);
+    synth.speak(utterThis);
+}
+
+
+function check(){
+    image=document.getElementById('captured_image');
+    classifier.classify(image,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById('result_gesture_name').innerHTML=results[0].label;
+        prediction=results[0].label;
+        speak();
+
+        if(results[0].label=="victory"){
+            document.getElementById("update_gesture").innerHTML="&#9996;";
+        }
+
+        if(results[0].label=="good"){
+            document.getElementById("update_gesture").innerHTML="&#128077;";
+        }
+
+        if(results[0].label=="bad"){
+            document.getElementById("update_gesture").innerHTML="&#128078;";
+        }
+
+        if(results[0].label=="outstanding"){
+            document.getElementById("update_gesture").innerHTML="&#128076;";
+        }
+
+        
+
+    }
+}
+
+
+
+
+
+
+
+
